d51deacf0bf85a833e55ce371596dc3a7db50101
Jordan DAFFIX <jodaffix@local.isima.fr>
2020-01-24 16:21:14 +0100

    Initial commit

---
+++ Isima.DTO/SceneDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isima.DTO
+{
+    public class SceneDto
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ isima.DAL/SceneRepository.cs
@@ -0,0 +1,50 @@
+﻿using Isima.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isima.DAL
+{
+    public class SceneRepository : IDisposable
+    {
+        private readonly IsimaEntities _dbcontext = null;
+
+        public SceneRepository()
+        {
+            _dbcontext = new IsimaEntities();
+        }
+
+        public SceneRepository(IsimaEntities context)
+        {
+            _dbcontext = context;
+        }
+
+
+        public List<SceneDto> GetScene(int ID)
+        {
+            try
+            {
+                List<Scene> sceneEntities = _dbcontext.Scene.Where(x => x.ID == ID).ToList();
+                return sceneEntities.Select(x => new SceneDto
+                {
+                    ID = x.ID,
+                    Content = x.Content,
+                    Title = x.Title
+                }).ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            _dbcontext.Dispose();
+        }
+    }
+}
